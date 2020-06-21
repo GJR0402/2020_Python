f694ca34d40e9c6d6eb9011df9cc51739d55e77d
GJR0402 <64392906+GJR0402@users.noreply.github.com>
2020-06-21 23:01:03 +0800

    Update creat random data.py
    平时作业1

--- 郭靖如2018012851/creat random data.py
+++ 郭靖如2018012851/creat random data.py
@@ -25,13 +25,35 @@ def dataSampling(datatype,datarange,num,strlen=6):
         else:
             continue
     return result
-def dataScreeing():
-    pass
+def dataScreening(data,*args):
+    Array=set()
+    try:
+        for j in data:
+            it=iter(args)
+            if type(j)is int and next(it)<=j<=next(it):
+                Array.add(j)
+            if type(j)is float and next(it)<=j<=next(it):
+                Array.add(j)
+            if type(j) is str and next(it) in j:
+                Array.add(j)
+    except ValueError:
+        print("参数有误")
+    except TypeError:
+        print("类型有误")
+    except Exception as e:
+        print(e)
+    return Array
 def apply():
     result1=dataSampling(int,[1,100],100)
     print(result1)
+    r1_screening = dataScreening(result1, 20, 60)
+    print(r1_screening)
     result2=dataSampling(float,[1,100],50)
     print(result2)
+    r2_screening = dataScreening(result2, 80, 100)
+    print(r2_screening)
     result3=dataSampling(str,"abcdefg",5)
     print(result3)
+    r3_screening = dataScreening(result3, "ab")
+    print(r3_screening)
 apply()
